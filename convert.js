6d799a2e252945a05dea38add1f4743860c3794f
TheRealMoaki <32156870+TheRealMoaki@users.noreply.github.com>
2017-09-28 07:44:03 +0200

    Add files via upload

--- convert.js
+++ convert.js
@@ -1,1 +1,12 @@
-console.log('Not implemented yet!');
+var convert = require('convert-units');
+        
+try {    
+    var unit = convert(parseInt(process.argv[2])).from(process.argv[3]).to(process.argv[5]);
+    unit ? console.log(unit) : handleArgumentError();     
+} catch (ex) {
+    handleArgumentError();
+}
+
+function handleArgumentError() {
+    console.error('Invalid parameters');
+}
