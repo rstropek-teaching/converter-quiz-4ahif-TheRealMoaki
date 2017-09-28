20e919cf64247a0c1f277e38810b342fca3555c1
Rainer Stropek <rainer@timecockpit.com>
2017-09-28 09:32:48 +0200

    Add tips

--- convert.js
+++ convert.js
@@ -2,6 +2,8 @@ var convert = require('convert-units');
         
 try {    
     var unit = convert(parseInt(process.argv[2])).from(process.argv[3]).to(process.argv[5]);
+    // The following console.log statement does not fulfill the exact requirements. Therefore, tests are not green yet.
+    // However, the conversion itself is done correctly.
     unit ? console.log(unit) : handleArgumentError();     
 } catch (ex) {
     handleArgumentError();
